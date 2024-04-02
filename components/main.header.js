dacd39fe20c25a5cb8e2745a3c9b4e0557b4ea1c
Sanaz Soltani <74157401+Snzslt@users.noreply.github.com>
2024-04-02 16:05:55 -0400

    Using more custom component

--- components/main.header.js
+++ components/main.header.js
@@ -1,26 +1,31 @@
 import Link from 'next/link';
 import Image from 'next/image';
+
+import MainHeaderBackground from './main-header-backgroung';
 import logoImg from '@/assets/logo.png';
-import classes from './main-header.module.css'
+import classes from './main-header.module.css';
 
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
 
-export default function MainHeader(){
-    return(
-        <header className={classes.header}>
-            <Link href="/" className={classes.logo}>
-                <Image src={logoImg.src} alt="A Plate with food on it" priority/> 
-                Next Level Food
-            </Link>
-            <nav className={classes.nav}>
-                <ul>
-                    <li>
-                    <Link href="/meals">Browse Meals</Link>
-                    </li>
-                    <li>
-                    <Link href="/community">Foodie Community</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
